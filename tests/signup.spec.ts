6295c8e360c6a047a9aff3110dd313b28b16bb6c
Jamie <jamie@home.com>
2024-07-16 18:48:14 +0100

    adds account creation test

--- tests/signup.spec.ts
+++ tests/signup.spec.ts
@@ -9,5 +9,18 @@ test("Users can create new accounts", async ({ page }) => {
   await page.getByTestId('password').fill('newuser@dev.com');
   await page.getByTestId('password-repeat').fill('newuser@dev.com');
   await page.getByTestId('signup-submit').click();
+  await page.waitForURL('**/signup');
   await expect(page).toHaveURL('/signup/confirm?email=newuser@dev.com');
+});
+
+test("Attempting to create an already existing account shows a message", async ({ page }) => {
+  await page.goto('/signup');
+  await page.waitForURL('/signup');
+  await page.getByTestId('firstname').fill('Tormund');
+  await page.getByTestId('lastname').fill('Giantsbane');
+  await page.getByTestId('email').fill('eventfan@dev.com');
+  await page.getByTestId('password').fill('supersecret');
+  await page.getByTestId('password-repeat').fill('supersecret');
+  await page.getByTestId('signup-submit').click();
+  await expect(page.getByText('User already registered')).toBeVisible();
 });
