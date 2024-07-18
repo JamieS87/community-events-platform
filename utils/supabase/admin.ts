589bc631865e827871c8533835906dad1933b673
Jamie <jamie@home.com>
2024-07-18 15:56:14 +0100

    adds updateSupabaseCustomer function

--- utils/supabase/admin.ts
+++ utils/supabase/admin.ts
@@ -1,7 +1,8 @@
+import { Database } from "@/dbtypes";
 import { createClient, User } from "@supabase/supabase-js";
 import Stripe from "stripe";
 
-const supabaseAdmin = createClient(
+const supabaseAdmin = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!!
 );
@@ -11,12 +12,13 @@ export const selectSupabaseCustomerIdByUserId = async (userId: User["id"]) => {
     .from("customers")
     .select("customer_id")
     .eq("user_id", userId)
+    .limit(1)
     .maybeSingle();
 
   if (error) {
     throw error;
   }
-  return data?.customer_id;
+  return data === null ? null : data.customer_id;
 };
 
 export const selectSupabaseUserIdByCustomerId = async (
@@ -43,6 +45,18 @@ export const insertSupabaseCustomer = async (
   }
 };
 
+export const updateSupabaseCustomer = async (
+  user_id: User["id"],
+  customer_id: Stripe.Customer["id"]
+) => {
+  const { error } = await supabaseAdmin
+    .from("customers")
+    .update({ customer_id }).eq('user_id',user_id);
+  if (error) {
+    throw error;
+  }
+};
+
 export const insertSupabasePurchasedEvent = async (
   user_id: User["id"],
   event_id: string,
@@ -51,7 +65,7 @@ export const insertSupabasePurchasedEvent = async (
 ) => {
   const { data: purchasedEvent, error } = await supabaseAdmin
     .from("purchased_events")
-    .insert({ user_id, event_id, wh_event_id, cs_id });
+    .insert({ user_id, event_id: Number(event_id), wh_event_id, cs_id });
   if (error) {
     throw error;
   }
