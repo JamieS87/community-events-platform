9b2daa2441abf5daf304613deee8dc91f6595537
Jamie <jamie@home.com>
2024-07-18 20:25:53 +0100

    adds loadingIcon prop that shows a loading icon when pending if true

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -2,19 +2,28 @@
 
 import { useFormStatus } from "react-dom";
 import { Button, ButtonProps } from "./ui/button";
+import { Loader2 } from "lucide-react";
 
 type Props = ButtonProps & {
   pendingText?: string;
+  loadingIcon?: boolean;
 };
 
-export function SubmitButton({ children, pendingText, ...props }: Props) {
+export function SubmitButton({
+  children,
+  pendingText,
+  loadingIcon = false,
+  ...props
+}: Props) {
   const { pending, action } = useFormStatus();
-
   const isPending = pending && action === props.formAction;
 
   return (
     <Button asChild variant={props.variant || "default"}>
       <button {...props} type="submit" aria-disabled={pending}>
+        {isPending && loadingIcon && (
+          <Loader2 className="mr-2 w-4 h-4 animate-spin" />
+        )}
         {isPending ? pendingText : children}
       </button>
     </Button>
