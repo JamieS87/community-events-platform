f30b787407070ad2c4551418e80690f202f3a1ef
Jamie <jamie@home.com>
2024-07-18 15:55:36 +0100

    updates component to handle events with the pay as you feel pricing model

--- components/purchase-event-button.tsx
+++ components/purchase-event-button.tsx
@@ -3,33 +3,85 @@
 import { User } from "@supabase/supabase-js";
 import { Button } from "./ui/button";
 import { usePathname, useRouter } from "next/navigation";
+import { Database } from "@/dbtypes";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "./ui/input";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  Form,
+  FormMessage,
+} from "@/components/ui/form";
+
+const payfSchema = z.object({
+  payf_price: z.coerce.number({ message: "price must be a number" }).optional(),
+  event_id: z.string(),
+});
 
 type PurchaseEventButtonProps = {
   user: User | null;
-  event: Record<string, any>;
+  event: Database["public"]["Tables"]["events"]["Row"];
+  [k: string]: any;
 };
 
 export default function PurchaseEventButton({
   user,
   event,
+  ...props
 }: PurchaseEventButtonProps) {
   const router = useRouter();
   const pathname = usePathname();
+
+  const isPayfEvent = event.pricing_model === "payf";
+
+  const form = useForm<z.infer<typeof payfSchema>>({
+    mode: "all",
+    resolver: zodResolver(payfSchema),
+    defaultValues: {
+      payf_price: 0.3,
+      event_id: String(event.id),
+    },
+  });
+
   return (
-    <form action="/events/checkout" method="POST">
-      <input type="hidden" name="event_id" value={event.id} />
-      <Button
-        onClick={async (e) => {
-          if (!user) {
-            e.preventDefault();
-            return router.replace(`/login?next=${pathname}`);
-          } else {
-            e.currentTarget.form?.requestSubmit();
-          }
-        }}
-      >
-        Buy
-      </Button>
-    </form>
+    <Form {...form}>
+      <form action="/events/checkout" method="POST">
+        {isPayfEvent && (
+          <FormField
+            control={form.control}
+            name="payf_price"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel className="text-foreground">PAYF price</FormLabel>
+                  <FormControl>
+                    <Input type="number" placeholder="1.00" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          ></FormField>
+        )}
+        <input type="hidden" name="event_id" value={event.id} />
+        <Button
+          disabled={!form.formState.isValid}
+          {...props}
+          data-testid="purchase-event"
+          onClick={async (e) => {
+            if (!user) {
+              e.preventDefault();
+              return router.replace(`/login?next=${pathname}`);
+            }
+          }}
+        >
+          Buy
+        </Button>
+      </form>
+    </Form>
   );
 }
