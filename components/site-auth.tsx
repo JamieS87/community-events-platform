710fde6df5677c924c35435b53f9ff4762fdd85b
Jamie <jamie@home.com>
2024-07-20 15:17:03 +0100

    removes unused import

--- components/site-auth.tsx
+++ components/site-auth.tsx
@@ -1,7 +1,7 @@
 import { createClient } from "@/utils/supabase/server";
 import { Button } from "./ui/button";
 import Link from "next/link";
-import AuthUserMenu from "./auth-user-menu";
+
 import {
   DropdownMenu,
   DropdownMenuItem,
