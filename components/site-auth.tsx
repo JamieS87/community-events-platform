3920f243906bb442d498a95fc805774ffc66c54f
Jamie <jamie@home.com>
2024-07-20 15:10:39 +0100

    updates component to show dropdown user menu when authenticated

--- components/site-auth.tsx
+++ components/site-auth.tsx
@@ -2,6 +2,16 @@ import { createClient } from "@/utils/supabase/server";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import AuthUserMenu from "./auth-user-menu";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import UserAvatar from "./user-avatar";
+import { DropdownMenuContent } from "./ui/dropdown-menu";
+import { hasStaffClaim } from "@/app/lib/utils";
+import { User } from "lucide-react";
+import SignOutButton from "./sign-out-button";
 
 export default async function SiteAuth() {
   const supabase = createClient();
@@ -10,9 +20,37 @@ export default async function SiteAuth() {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
+
+  const isStaff = session && hasStaffClaim(session);
+
   return user ? (
-    <AuthUserMenu user={user} />
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserAvatar user={user} />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {isStaff && (
+          <DropdownMenuItem asChild>
+            <Link href="/admin">Admin Dashboard</Link>
+          </DropdownMenuItem>
+        )}
+        <DropdownMenuItem asChild>
+          <Link href="/profile" className="flex items-center">
+            <User className="w-4 h-4 mr-2" />
+            Profile
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <SignOutButton />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   ) : (
+    // <AuthUserMenu user={user} />
     <Button asChild>
       <Link href="/login" data-testid="login">
         Login
