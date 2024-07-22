cb0ecd107976151f0cd461f8037ef65224910f8e
Jamie <jamie@home.com>
2024-07-22 16:44:11 +0100

    adds link google account button

---
+++ components/link-google-account-button.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { requestLinkGoogleIdentity } from "@/app/lib/actions/auth";
+import GoogleIcon from "@/components/google-icon";
+import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
+
+export default function LinkGoogleAccount() {
+  const pathname = usePathname();
+  return (
+    <Button
+      variant={"outline"}
+      className="flex items-center"
+      onClick={() => {
+        requestLinkGoogleIdentity(pathname);
+      }}
+    >
+      <div className="w-6 h-6 mr-4">
+        <GoogleIcon />
+      </div>
+      Link Google Account
+    </Button>
+  );
+}
