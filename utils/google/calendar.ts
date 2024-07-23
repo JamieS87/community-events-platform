63b29ef69eb647d16503b06e6e267b1d80932b15
Jamie <jamie@home.com>
2024-07-23 14:30:26 +0100

    refactor

--- utils/google/calendar.ts
+++ utils/google/calendar.ts
@@ -1,23 +1,14 @@
-import { Tables } from "@/dbtypes";
-import { calendar_v3 } from "@googleapis/calendar";
+import { calendar } from "@googleapis/calendar";
+import { createGoogleServerClient } from "./server";
 
-export async function insertGoogleCalendarEvent(
-  calendar: calendar_v3.Calendar,
-  event: Tables<"events">
-) {
-  const result = await calendar.events.insert({
-    calendarId: "primary",
-    requestBody: {
-      summary: event.name,
-      start: {
-        timeZone: "UTC",
-        dateTime: `${event.start_date}T${event.start_time}`,
-      },
-      end: {
-        timeZone: "UTC",
-        dateTime: `${event.end_date}T${event.end_time}`,
-      },
-    },
-  });
-  return result;
-}
+export const createCalendarClient = async (
+  access_token: string,
+  refresh_token: string
+) => {
+  const googleClient = await createGoogleServerClient(
+    access_token,
+    refresh_token
+  );
+  const client = calendar({ version: "v3", auth: googleClient });
+  return client;
+};
