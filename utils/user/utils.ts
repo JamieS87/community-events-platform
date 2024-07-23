6becf2282888f5b5c9d4de9d2f83327b6e07de09
Jamie <jamie@home.com>
2024-07-23 14:21:38 +0100

    formatting

--- utils/user/utils.ts
+++ utils/user/utils.ts
@@ -2,13 +2,11 @@ import { User } from "@supabase/supabase-js";
 
 export function getUserFullName(user: User) {
   const { user_metadata } = user;
-  if(user_metadata.name) {
+  if (user_metadata.name) {
     return user_metadata.name;
   } else if (user_metadata.full_name) {
     return user_metadata.full_name;
-  } else if (
-    user_metadata.first_name && user_metadata.last_name
-  ) {
-    return user_metadata.first_name + ' ' + user_metadata.last_name;
+  } else if (user_metadata.first_name && user_metadata.last_name) {
+    return user_metadata.first_name + " " + user_metadata.last_name;
   }
 }
