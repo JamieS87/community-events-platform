cbc206bfdc29158c9b26ffa66ac3294f69dd8031
Jamie <jamie@home.com>
2024-07-24 11:39:56 +0100

    adds postgres function that create a secret on profiles when a user is created, adds postgres functions for getting and setting a users google refresh token

--- supabase/migrations/20240703122521_create_user_insert_profile_trigger.sql
+++ supabase/migrations/20240703122521_create_user_insert_profile_trigger.sql
@@ -1,19 +1,18 @@
 create or replace function public.create_user_profile()
 returns trigger
+security definer
 language plpgsql
 as
 $$
+declare
+  secretId uuid;
 begin
-  insert into public.profiles (user_id, is_staff)
-    values (new.id, false);
+  select into secretId vault.create_secret('');
+  insert into public.profiles (user_id, is_staff, googleRefreshToken)
+    values (new.id, false, secretId);
   return new;
 end;
 $$;
-
-create or replace trigger on_insert_create_user_profile
-after insert on auth.users
-for each row
-execute function public.create_user_profile();
 
 grant execute
   on function public.create_user_profile
@@ -22,3 +21,59 @@ grant execute
 revoke execute
   on function public.create_user_profile
   from authenticated, anon, public;
+
+create or replace function public.set_google_refresh_token(refresh_token text)
+returns void
+security definer
+language plpgsql
+as
+$$
+declare
+  secretId uuid;
+begin
+  select googleRefreshToken into secretId from
+    public.profiles
+    where user_id = ( auth.uid() );
+  update vault.secrets
+    set secret = refresh_token
+    where vault.secrets.id = secretId;
+end;
+$$;
+
+grant execute 
+  on function public.set_google_refresh_token
+  to authenticated;
+
+revoke execute
+  on function public.set_google_refresh_token
+  from anon, public;
+
+create or replace function public.get_google_refresh_token()
+returns text
+security definer
+language plpgsql
+as
+$$
+declare
+  result text;
+begin
+  select decrypted_secret into result 
+    from vault.decrypted_secrets
+    join public.profiles on googleRefreshToken = vault.decrypted_secrets.id
+    where public.profiles.user_id = ( auth.uid() );
+  return result;
+end;
+$$;
+
+grant execute
+  on function public.get_google_refresh_token
+  to authenticated;
+
+revoke execute
+  on function public.get_google_refresh_token
+  from anon, public;
+
+create or replace trigger on_insert_create_user_profile
+after insert on auth.users
+for each row
+execute function public.create_user_profile();
