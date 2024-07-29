3bc955a9871097fd435ef564fdd745661c00272e
Jamie <jamie@home.com>
2024-07-29 15:15:43 +0100

    modifies admin page to show events in descending order of creation date, changes layout and adds functionality

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,30 +1,66 @@
-import { Button } from "@/components/ui/button";
 import { createClient } from "@/utils/supabase/server";
-import { PlusIcon } from "lucide-react";
+import { CreateEventForm } from "@/components/forms/create-event-form";
+import { format } from "date-fns";
+import Link from "next/link";
+import AdminEventMenu from "@/components/admin-event-menu";
+import PublishEventButton from "@/components/publish-event-button";
+import UnpublishEventButton from "@/components/unpublish-event-button";
 
 export default async function AdminPage() {
   const supabase = createClient();
-  const { data: events, error } = await supabase.from("events").select("*");
+  const { data: events, error } = await supabase
+    .from("events")
+    .select("*")
+    .order("created_at", { ascending: false })
+    .order("id", { ascending: false });
   if (error) {
     throw error;
   }
 
   return (
-    <div className="flex flex-col space-y-4 w-full p-2">
+    <div className="flex flex-col space-y-4 w-full p-2 max-w-6xl">
       <div className="flex w-full items-center justify-between">
         <h2>Events</h2>
-        <Button>
-          <PlusIcon className="w-6 h-6" />
-          Add Event
-        </Button>
+        <CreateEventForm />
       </div>
-      <ul className="w-full">
+      <ul className="w-full grid-cols-5 divide-y space-y-2 text-sm">
+        <li className="items-center pt-2 text-center grid grid-cols-8 font-semibold">
+          <span>Name</span>
+          <span>Pricing Model</span>
+          <span>Price</span>
+          <span>Published</span>
+          <span>--</span>
+          <span>Start Date</span>
+          <span>End Date</span>
+        </li>
         {events.map((event) => {
           return (
-            <li key={event.id} className="flex items-center">
-              <h3 className="flex-1">{event.name}</h3>
-              <p className="flex-1">{event.description}</p>
+            <li
+              key={event.id}
+              className="items-center pt-2 text-center grid grid-cols-8"
+            >
+              <Link href={`/events/${event.id}`}>
+                <h3 className="flex-1">{event.name}</h3>
+              </Link>
+              <p className="flex-1">{event.pricing_model}</p>
+              <p className="flex-1">
+                {event.pricing_model === "payf"
+                  ? "N/A"
+                  : `£${event.price / 100}`}
+              </p>
               <p className="flex-1">{String(event.published)}</p>
+              <p className="flex-1">
+                {event.published ? (
+                  <UnpublishEventButton eventId={event.id} />
+                ) : (
+                  <PublishEventButton eventId={event.id} />
+                )}
+              </p>
+              <p className="flex-1">{format(event.start_date, "PPP")}</p>
+              <p className="flex-1">{format(event.end_date, "PPP")}</p>
+              <div>
+                <AdminEventMenu eventId={event.id} />
+              </div>
             </li>
           );
         })}
