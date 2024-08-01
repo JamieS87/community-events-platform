e1dd4e71860fd7cb1f0ff9fdd088a48bea444197
Jamie <jamie@home.com>
2024-08-01 13:49:37 +0100

    adds images remotePattern to allow loading images with the nextjs Image tag

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "127.0.0.1",
+        protocol: "http",
+        port: "54321",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
