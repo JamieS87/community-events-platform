8656432069138c14c617a57937e35e477e181de8
Jamie <jamie@home.com>
2024-08-02 12:41:29 +0100

    adds functions for retrieving events

--- utils/events/server.ts
+++ utils/events/server.ts
@@ -5,6 +5,58 @@ export const getLatestEvents = async (limit: number) => {
   const { data, error } = await supabase
     .from("events")
     .select("id,name,pricing_model,thumbnail")
+    .eq("published", true)
+    .order("created_at", { ascending: false })
+    .order("id")
+    .limit(limit);
+
+  if (error) {
+    throw error;
+  }
+  return data;
+};
+
+export const getLatestFreeEvents = async (limit: number) => {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("events")
+    .select("id,name,pricing_model,thumbnail")
+    .eq("published", true)
+    .eq("pricing_model", "free")
+    .order("created_at", { ascending: false })
+    .order("id")
+    .limit(limit);
+
+  if (error) {
+    throw error;
+  }
+  return data;
+};
+
+export const getLatestPAYFEvents = async (limit: number) => {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("events")
+    .select("id,name,pricing_model,thumbnail")
+    .eq("published", true)
+    .eq("pricing_model", "payf")
+    .order("created_at", { ascending: false })
+    .order("id")
+    .limit(limit);
+
+  if (error) {
+    throw error;
+  }
+  return data;
+};
+
+export const getLatestPaidEvents = async (limit: number) => {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("events")
+    .select("id,name,pricing_model,thumbnail")
+    .eq("published", true)
+    .eq("pricing_model", "paid")
     .order("created_at", { ascending: false })
     .order("id")
     .limit(limit);
