eb210a1bd49debde0d654137303573b86d1d2f0e
Jamie <jamie@home.com>
2024-08-02 11:16:31 +0100

    refactors code to handle paid or free purchases, removing functionality for PAYF event purchases

--- components/purchase-event-button.tsx
+++ components/purchase-event-button.tsx
@@ -6,20 +6,11 @@ import { Database } from "@/dbtypes";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Input } from "./ui/input";
-import {
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  Form,
-  FormMessage,
-} from "@/components/ui/form";
+import { Form } from "@/components/ui/form";
 import { purchaseEvent } from "@/app/lib/actions/checkout";
 import { SubmitButton } from "./submit-button";
 
-const payfSchema = z.object({
-  payf_price: z.coerce.number({ message: "price must be a number" }).optional(),
+const formSchema = z.object({
   event_id: z.string(),
 });
 
@@ -37,13 +28,10 @@ export default function PurchaseEventButton({
   const router = useRouter();
   const pathname = usePathname();
 
-  const isPayfEvent = event.pricing_model === "payf";
-
-  const form = useForm<z.infer<typeof payfSchema>>({
+  const form = useForm<z.infer<typeof formSchema>>({
     mode: "all",
-    resolver: zodResolver(payfSchema),
+    resolver: zodResolver(formSchema),
     defaultValues: {
-      payf_price: 0.3,
       event_id: String(event.id),
     },
   });
@@ -51,23 +39,6 @@ export default function PurchaseEventButton({
   return (
     <Form {...form}>
       <form className="w-full">
-        {isPayfEvent && (
-          <FormField
-            control={form.control}
-            name="payf_price"
-            render={({ field }) => {
-              return (
-                <FormItem>
-                  <FormLabel className="text-foreground">PAYF price</FormLabel>
-                  <FormControl>
-                    <Input type="number" placeholder="1.00" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          ></FormField>
-        )}
         <input type="hidden" name="event_id" value={event.id} />
         <SubmitButton
           className="w-full"
