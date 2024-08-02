cc00d302139e3e95bb79e98e058d2a6611576fe1
Jamie <jamie@home.com>
2024-08-02 11:07:17 +0100

    changes layout

--- components/index-header.tsx
+++ components/index-header.tsx
@@ -1,6 +1,6 @@
 export default function IndexHeader() {
   return (
-    <div className="w-full min-h-[70dvh] flex flex-col justify-center items-center gap-10">
+    <div className="w-full min-h-[60dvh] flex flex-col justify-center items-center gap-10">
       <h2 className="font-bold text-5xl">Events Platform</h2>
       <p className="text-xl">Discover free, paid, and pay-as-you-feel events</p>
     </div>
