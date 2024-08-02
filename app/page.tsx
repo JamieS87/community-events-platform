9ffbf68ea0fd9710b13ee01fc82419471dfc9b11
Jamie <jamie@home.com>
2024-08-02 12:41:13 +0100

    updates index page to use new events list components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import EventCard, { LoadingEventCard } from "@/components/event-card";
+import {
+  EventsListHeader,
+  EventsListItems,
+  LoadingEventsListItems,
+} from "@/components/events-list";
 import IndexHeader from "@/components/index-header";
 
 import {
-  LatestEventsSection,
-  LoadingLatestEventsSection,
-} from "@/components/latest-events-section";
+  getLatestEvents,
+  getLatestFreeEvents,
+  getLatestPaidEvents,
+  getLatestPAYFEvents,
+} from "@/utils/events/server";
 import { Suspense } from "react";
 
 export default async function Index() {
@@ -12,9 +20,126 @@ export default async function Index() {
       <div className="w-full flex flex-col gap-20 px-2">
         <IndexHeader />
         <main className="w-full flex flex-col gap-6">
-          <Suspense fallback={<LoadingLatestEventsSection />}>
-            <LatestEventsSection headingText="Recently added" />
-          </Suspense>
+          {/* Latest events */}
+          <section>
+            <Suspense
+              fallback={
+                <>
+                  <EventsListHeader loading={true} />
+                  <LoadingEventsListItems
+                    className="grid grid-cols-3 gap-4"
+                    count={5}
+                    render={() => {
+                      return <LoadingEventCard />;
+                    }}
+                  />
+                </>
+              }
+            >
+              <EventsListHeader>Latest events</EventsListHeader>
+              <EventsListItems
+                className="grid grid-cols-3 gap-4"
+                getter={getLatestEvents.bind(null, 5)}
+                render={({ event }) => {
+                  return (
+                    <li key={event.id}>
+                      <EventCard event={event} />
+                    </li>
+                  );
+                }}
+              />
+            </Suspense>
+          </section>
+          {/* Latest paid events */}
+          <section>
+            <Suspense
+              fallback={
+                <>
+                  <EventsListHeader loading={true} />
+                  <LoadingEventsListItems
+                    className="grid grid-cols-3 gap-4"
+                    count={5}
+                    render={() => {
+                      return <LoadingEventCard />;
+                    }}
+                  />
+                </>
+              }
+            >
+              <EventsListHeader>Paid events</EventsListHeader>
+              <EventsListItems
+                className="grid grid-cols-3 gap-4"
+                getter={getLatestPaidEvents.bind(null, 5)}
+                render={({ event }) => {
+                  return (
+                    <li key={event.id}>
+                      <EventCard event={event} />
+                    </li>
+                  );
+                }}
+              />
+            </Suspense>
+          </section>
+          {/* Latest PAYF Events */}
+          <section>
+            <Suspense
+              fallback={
+                <>
+                  <EventsListHeader loading={true} />
+                  <LoadingEventsListItems
+                    className="grid grid-cols-3 gap-4"
+                    count={5}
+                    render={() => {
+                      return <LoadingEventCard />;
+                    }}
+                  />
+                </>
+              }
+            >
+              <EventsListHeader>Pay as you feel events</EventsListHeader>
+              <EventsListItems
+                className="grid grid-cols-3 gap-4"
+                getter={getLatestPAYFEvents.bind(null, 5)}
+                render={({ event }) => {
+                  return (
+                    <li key={event.id}>
+                      <EventCard event={event} />
+                    </li>
+                  );
+                }}
+              />
+            </Suspense>
+          </section>
+          {/* Latest free events */}
+          <section>
+            <Suspense
+              fallback={
+                <>
+                  <EventsListHeader loading={true} />
+                  <LoadingEventsListItems
+                    className="grid grid-cols-3 gap-4"
+                    count={5}
+                    render={() => {
+                      return <LoadingEventCard />;
+                    }}
+                  />
+                </>
+              }
+            >
+              <EventsListHeader>Free events</EventsListHeader>
+              <EventsListItems
+                className="grid grid-cols-3 gap-4"
+                getter={getLatestFreeEvents.bind(null, 5)}
+                render={({ event }) => {
+                  return (
+                    <li key={event.id}>
+                      <EventCard event={event} />
+                    </li>
+                  );
+                }}
+              />
+            </Suspense>
+          </section>
         </main>
       </div>
     </div>
