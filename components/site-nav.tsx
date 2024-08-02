dafcd3679fc69f1d0591a096df602133c042a480
Jamie <jamie@home.com>
2024-08-02 11:07:26 +0100

    chnegs layout

--- components/site-nav.tsx
+++ components/site-nav.tsx
@@ -3,16 +3,18 @@ import SiteAuth from "./site-auth";
 
 export default async function SiteNav() {
   return (
-    <nav className="w-full max-w-6xl mx-auto flex items-center justify-center p-2">
+    <nav className="w-full flex items-center justify-center border-b">
       {/* left */}
-      <div className="w-full flex items-center">
-        <div className="flex-1 flex items-center">
-          <Link href="/" className="font-bold text-2xl">
-            CEP
-          </Link>
-        </div>
-        <div className="flex-1 flex items-center justify-end">
-          <SiteAuth />
+      <div className="w-full max-w-7xl mx-auto flex items-center justify-center p-2">
+        <div className="w-full flex items-center">
+          <div className="flex-1 flex items-center">
+            <Link href="/" className="font-bold text-2xl">
+              CEP
+            </Link>
+          </div>
+          <div className="flex-1 flex items-center justify-end">
+            <SiteAuth />
+          </div>
         </div>
       </div>
     </nav>
