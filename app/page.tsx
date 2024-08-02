9ddd0c82aadd00ce97ec99ff63917047e866b758
Jamie <jamie@home.com>
2024-08-02 11:15:12 +0100

    updates page to use LatestEventsSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
-import IndexEventsList from "@/components/index-events-list";
 import IndexHeader from "@/components/index-header";
+
+import {
+  LatestEventsSection,
+  LoadingLatestEventsSection,
+} from "@/components/latest-events-section";
 import { Suspense } from "react";
 
 export default async function Index() {
   return (
-    <div className="w-full max-w-6xl mx-auto flex flex-col gap-20">
+    <div className="w-full max-w-7xl mx-auto flex flex-col gap-20">
       <div className="w-full flex flex-col gap-20 px-2">
         <IndexHeader />
         <main className="w-full flex flex-col gap-6">
-          <section className="w-full">
-            <Suspense fallback={<div>Loading events...</div>}>
-              <IndexEventsList />
-            </Suspense>
-          </section>
+          <Suspense fallback={<LoadingLatestEventsSection />}>
+            <LatestEventsSection headingText="Recently added" />
+          </Suspense>
         </main>
       </div>
     </div>
