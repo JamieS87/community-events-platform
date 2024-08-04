3fc184ab2b17a7517ae99c6c8c6a4b06d9564910
Jamie <jamie@home.com>
2024-08-04 11:17:27 +0100

    changes purchased events table to include event columns rather than referencing an event

--- supabase/migrations/20240709191830_create_purchased_events_table.sql
+++ supabase/migrations/20240709191830_create_purchased_events_table.sql
@@ -1,7 +1,16 @@
 create table purchased_events (
   id bigint primary key generated always as identity,
   user_id uuid not null references auth.users (id) on delete cascade,
-  event_id bigint not null references public.events (id),
+  event_id bigint not null,
+  name varchar(256),
+  description varchar(256),
+  start_date date,
+  end_date date,
+  start_time time,
+  end_time time,
+  pricing_model public.pricing_model,
+  price bigint,
+  thumbnail text,
   wh_event_id text unique,
   cs_id text unique
 );
