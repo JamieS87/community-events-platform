e1c9a164bda98c10bb61769daadf0a1c03c69c4f
Jamie <jamie@home.com>
2024-08-17 12:42:25 +0100

    adds event page POMs

---
+++ tests/playwright-event-page.ts
@@ -0,0 +1,61 @@
+import { expect, type Locator, type Page } from "@playwright/test";
+
+export class FreeEventPage {
+  readonly page: Page;
+  readonly buyButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.buyButton = page.getByRole("button", { name: "Buy" });
+  }
+
+  async goto(path: string) {
+    await this.page.goto(path);
+    await expect(this.page.locator("div", { hasText: /^Free$/ })).toBeVisible();
+  }
+
+  async clickBuy() {
+    await this.buyButton.click();
+  }
+}
+
+export class PaidEventPage {
+  readonly page: Page;
+  readonly buyButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.buyButton = page.getByRole("button", { name: "Buy" });
+  }
+
+  async goto(path: string) {
+    await this.page.goto(path);
+    await expect(this.page.locator("div", { hasText: /^Paid$/ })).toBeVisible();
+  }
+
+  async clickBuy() {
+    await this.buyButton.click();
+  }
+}
+
+export class PAYFEventPage {
+  readonly page: Page;
+  readonly buyButton: Locator;
+  readonly priceInput: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.buyButton = page.getByRole("button", { name: "Buy" });
+    this.priceInput = page.getByRole("spinbutton", { name: "Price" });
+  }
+
+  async goto(path: string) {
+    await this.page.goto(path);
+    await expect(this.page.locator("div", { hasText: /^Payf$/ })).toBeVisible();
+  }
+
+  async clickBuy(price: string) {
+    await this.priceInput.fill(price);
+    await this.buyButton.click();
+  }
+}
