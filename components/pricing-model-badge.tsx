b38c7ef0e6876eb536b026742e3356e7731538e6
Jamie <jamie@home.com>
2024-08-18 13:26:17 +0100

    makes component a client component and changes styling

--- components/pricing-model-badge.tsx
+++ components/pricing-model-badge.tsx
@@ -1,16 +1,26 @@
+"use client";
+
 import { Tables } from "@/dbtypes";
 import { Badge, BadgeProps } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
 
 type PricingModelBadgeProps = {
   pricing_model: Tables<"events">["pricing_model"];
 } & BadgeProps;
 
-export default async function PricingModelBadge({
+export default function PricingModelBadge({
   pricing_model,
   ...props
 }: PricingModelBadgeProps) {
   return (
-    <Badge className="text-md" {...props}>
+    <Badge
+      className={cn(
+        "text-md",
+        "bg-sky-200 border-sky-400 text-sky-700 hover:bg-sky-200 hover:border-sky-400 hover:text-sky-700 hover:cursor-default",
+        props.className
+      )}
+      {...props}
+    >
       {pricing_model[0].toUpperCase() + pricing_model.slice(1)}
     </Badge>
   );
