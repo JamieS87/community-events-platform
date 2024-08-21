d08338e264dceec49146c476cb33d0bcc54c5d12
Jamie <jamie@home.com>
2024-08-21 21:30:03 +0100

    adds loading pages

---
+++ app/admin/loading.tsx
@@ -0,0 +1,30 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default async function Loading() {
+  return (
+    <Card className="mx-auto max-w-7xl w-full mt-8">
+      <CardHeader>
+        <CardTitle>
+          <span className="w-full h-8 bg-gray-100 animate-pulse"></span>
+        </CardTitle>
+        <CardDescription>
+          <span className="w-full h-8 bg-gray-100 animate-pulse"></span>
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <div className="flex flex-col gap-y-4">
+          <div className="w-[180px] h-6 bg-gray-100 animate-pulse"></div>
+          <div className="w-full h-6 bg-gray-100 animate-pulse"></div>
+          <div className="w-[180px] h-6 bg-gray-100 animate-pulse"></div>
+          <div className="w-full rounded-lg h-8 bg-gray-100 animate-pulse"></div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
