5bb0e9bce79340b72a00a8c3ad7d7a911fbba3be
Jamie <jamie@home.com>
2024-08-21 21:30:43 +0100

    adds info colour

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,9 +19,13 @@ const config = {
     },
     extend: {
       colors: {
+        info: {
+          DEFAULT: "hsl(var(--info))",
+          foreground: "hsl(var(--info-foreground))",
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
-        ring: "hsl(var(--primary))",
+        ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
